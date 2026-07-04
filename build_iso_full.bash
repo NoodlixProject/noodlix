d66cb9bb4c2e0ae3122c22cee3c872e18377460b
Johnny Konczal <johndrostan@outlook.com>
2026-07-04 12:25:36 -0700

    Copy system-rootfs/ to rootdata/ in installer build

--- build_iso_full.bash
+++ build_iso_full.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 cp -a system-initramfs/* installer_iso/boot/initramfs/installer/efidata/initramfs/
+cp -a system-rootfs/* installer_iso/boot/initramfs/installer/rootdata/
 
 ./pack_any_initramfs.bash installer_iso/boot/initramfs/installer/efidata/initramfs/ installer_iso/boot/initramfs/installer/efidata/initramfs.img
 
