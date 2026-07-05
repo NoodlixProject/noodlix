ff93b7998a0ebac0a04dfbb73adbff6cd0025d3b
Johnny Konczal <johndrostan@outlook.com>
2026-07-05 14:12:16 -0700

    Fix build_iso_full: create target dirs, handle empty sources

--- build_iso_full.bash
+++ build_iso_full.bash
@@ -1,7 +1,11 @@
 #! /bin/bash
+set -e
 
-cp -a system-initramfs/* installer_iso/boot/initramfs/installer/efidata/initramfs/
-cp -a system-rootfs/* installer_iso/boot/initramfs/installer/rootdata/
+mkdir -p installer_iso/boot/initramfs/installer/efidata/initramfs/
+cp -a system-initramfs/* installer_iso/boot/initramfs/installer/efidata/initramfs/ 2>/dev/null || true
+
+mkdir -p installer_iso/boot/initramfs/installer/rootdata/
+cp -a system-rootfs/* installer_iso/boot/initramfs/installer/rootdata/ 2>/dev/null || true
 
 ./pack_any_initramfs.bash installer_iso/boot/initramfs/installer/efidata/initramfs/ installer_iso/boot/initramfs/installer/efidata/initramfs.img
 
