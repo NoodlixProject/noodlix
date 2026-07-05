6c6cc07aa593e5d986af992fc50a5daef1ebffa4
Johnny Konczal <johndrostan@outlook.com>
2026-07-05 14:13:54 -0700

    Build nested initramfs from system-initramfs into efidata

--- build_iso_full.bash
+++ build_iso_full.bash
@@ -1,12 +1,14 @@
 #! /bin/bash
 set -e
 
-mkdir -p installer_iso/boot/initramfs/installer/efidata/initramfs/
-cp -a system-initramfs/* installer_iso/boot/initramfs/installer/efidata/initramfs/ 2>/dev/null || true
+# Copy system initramfs into efidata and pack it for the installed system
+mkdir -p installer_iso/boot/initramfs/installer/efidata/boot/initramfs/
+cp -a system-initramfs/* installer_iso/boot/initramfs/installer/efidata/boot/initramfs/ 2>/dev/null || true
+./pack_any_initramfs.bash installer_iso/boot/initramfs/installer/efidata/boot/initramfs/ installer_iso/boot/initramfs/installer/efidata/boot/initramfs.gz
 
+# Copy rootfs into rootdata/
 mkdir -p installer_iso/boot/initramfs/installer/rootdata/
 cp -a system-rootfs/* installer_iso/boot/initramfs/installer/rootdata/ 2>/dev/null || true
 
-./pack_any_initramfs.bash installer_iso/boot/initramfs/installer/efidata/initramfs/ installer_iso/boot/initramfs/installer/efidata/initramfs.img
-
+# Build the outer initramfs and ISO
 ./build.sh installer_iso noodlix.iso
